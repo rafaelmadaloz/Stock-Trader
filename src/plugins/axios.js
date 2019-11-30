f2d1cf23f091aff632eae2fce5f568c9f8a9d092
Rafael <madalozrafael@gmail.com>
2019-11-30 17:52:38 -0300

    Completed project

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 Vue.use({
     install(Vue) {
         Vue.prototype.$http = axios.create({
-            baseURL: 'https://curso-vue-faecf.firebaseio.com/'
+            baseURL: 'Your URL Base'
         })
     }
 })
